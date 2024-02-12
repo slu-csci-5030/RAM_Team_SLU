898bcce61860c5b4c1fdc5448ff2856c74faf002
Jahnavi-Uppugandla <jahnavi.u800@gmail.com>
2024-02-11 21:08:27 -0600

    Experiment blue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from
+ 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 function App() {
