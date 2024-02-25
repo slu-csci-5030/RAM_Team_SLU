3b8967d7e8055bd003764b48dc7e6bcba0a07823
Pawar Ashwin <pashwiny2k@gmail.com>
2024-02-25 01:09:36 -0600

    request access functionality 2

--- Frontend/src/components/SignUpmodal.jsx
+++ Frontend/src/components/SignUpmodal.jsx
@@ -1,31 +1,55 @@
 // SignUpModal.js
-import React, { useState } from 'react';
-import '../assets/Styles/Modal.css';
+import React, { useState } from "react";
+import "../assets/Styles/Modal.css";
 
 function SignUpModal({ onClose }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [fullName, setFullName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [fullName, setFullName] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("");
 
-  const handleSignUpSubmit = (e) => {
-    e.preventDefault();
-    
-    console.log('Sign Up form submitted with email:', email, 'password:', password, 'full name:', fullName, 'phone number:', phoneNumber);
-    onClose();
-  };
+	const handleSignUpSubmit = (e) => {
+		e.preventDefault();
 
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={onClose}>&times;</span>
-        <h2>Request Access</h2>
-        <form onSubmit={handleSignUpSubmit}>
-          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </form>
-      </div>
-    </div>
-  );
+		console.log(
+			"Sign Up form submitted with email:",
+			email,
+			"password:",
+			password,
+			"full name:",
+			fullName,
+			"phone number:",
+			phoneNumber
+		);
+		onClose();
+	};
+
+	return (
+		<div className="modal">
+			<div className="modal-content">
+				<span className="close" onClick={onClose}>
+					&times;
+				</span>
+				<h2>Request Access</h2>
+				<form onSubmit={handleSignUpSubmit}>
+					<input
+						type="email"
+						placeholder="Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<textarea
+						placeholder="Description"
+						value={fullName}
+						onChange={(e) => setFullName(e.target.value)}
+						rows={1}
+						cols={30}
+					/>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default SignUpModal;
