ab92241656b4e354fd01cbb01103ab4dfa333e52
Jahnavi-Uppugandla <jahnavi.u800@gmail.com>
2024-03-08 00:06:05 -0600

    updated route for login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,23 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+
 import Footer from './components/Footer';
+
+
 function App() {
   return (
-   <Router>
+    <Router>
       <div className="App">
         <Header />
         <Switch>
-           <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/loggedIn"  />
         </Switch>
         <Footer />
       </div>
-      </Router>
+    </Router>
 
   );
 }
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-// Home.js
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import CompanyDescription from '../components/CompanyDescription';
+
 function Home() {
+  const history = useHistory();
+
+  const handleLoginButtonClick = () => {
+    history.push('/loggedIn');
+  };
+
   return (
     <body>
-      <CompanyDescription />
+      <CompanyDescription onLoginButtonClick={handleLoginButtonClick} />
     </body>
-  )
+  );
 }
+
 export default Home;
