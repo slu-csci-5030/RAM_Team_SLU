9da6a6aee0801952a0dc67b68a43180b7d0095a3
jacksayshi <santoshiragavarapu@gmail.com>
2024-03-08 00:43:20 -0600

    edit functionality

---
+++ Frontend/src/components/EditAsset.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+//import '../assets/Styles/EditAsset.css';
+
+function EditAsset({ asset, onEdit, onSave }) {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    onEdit({ ...asset, [name]: value });
+  };
+
+  return (
+    <div className="edit-modal">
+      <div className="edit-modal-content">
+        <input
+          type="text"
+          name="assetName"
+          value={asset.assetName}
+          onChange={handleChange}
+          className="edit-asset-input"
+        />
+        <input
+          type="text"
+          name="location"
+          value={asset.location}
+          onChange={handleChange}
+          className="edit-asset-input"
+        />
+        <input
+          type="text"
+          name="quantity"
+          value={asset.quantity}
+          onChange={handleChange}
+          className="edit-asset-input"
+        />
+        <button onClick={onSave} className="save-asset-button">Save</button>
+      </div>
+    </div>
+  );
+}
+
+export default EditAsset;
