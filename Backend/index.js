fbd0d1dacfc8be23ef385f9685538d5af8e51db4
Manikanta Mogali <kasiviswanadh.mogali@slu.edu>
2024-03-27 11:39:08 -0500

    Delete asset Api code

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,17 @@
 import { PORT } from "./config.js";
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello, RAM Team!</h1>");
 });
+
+app.post("/assets", (req, res) => {
+    // Handle the POST request for /assets here
+    res.send("POST request to /assets deleted");
+});
+
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
