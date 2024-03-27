57c1814cb739df9775f422e87feccbec2a5f998d
amanohar1 <aakash.manohar@slu.edu>
2024-03-27 10:29:55 -0500

    Changes to assetModel.js and assetRoutes.js for use of DB

--- Backend/models/assetModel.js
+++ Backend/models/assetModel.js
@@ -1,47 +1,53 @@
-//this is coded for an imaginary database FYI
-class Asset {
-    constructor(data) {
-      // These are the fields based on dummy data
-      this.id = data.id;
-      this.name = data.name;
-      this.description = data.description;
-      // ... all other fields from your dummy data
-    }
-  
-    // Method to simulate saving the asset to a database
-    static save(data) {
-      // Since there's no database, we'll just return a new asset instance
-      return new Asset(data);
-    }
-  
-    // Method to simulate finding an asset by ID
-    static findById(id) {
-      // In a real scenario, we'd query the database.
-      // Here, we just return dummy data for the given ID
-      return new Asset(getDummyData(id));
-    }
-  
-    // Method to simulate updating an asset
-    update(data) {
-      // Apply the updates to this instance
-      Object.assign(this, data);
-      // In a real scenario, we'd save the updates to the database.
-      // Since there's no database, we'll just assume the update is successful.
-    }
-  
-    // Other static or instance methods would go here
-  }
-  
-  // A function to return dummy data based on ID
-  function getDummyData(id) {
-    // This would be replaced with database logic in a real application.
-    // For now, it's returning a fixed set of data.
-    return {
-      "id": id,
-      "name": "sample name",
-      // ... all other fields as in your assets.js file
-    };
-  }
-  
-  export default Asset;
-  
+// Importing Mongoose
+const mongoose = require('mongoose');
+
+// Asset Schema Definition
+const assetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  codedIn: String,
+  contacts: [String],
+  dataInput: String,
+  dataOutput: String,
+  addresses: [String],
+  url: String,
+  developedBy: [String],
+  inventoryNumber: String,
+  manufacturers: [String],
+  modelNumber: String,
+  feeForService: Boolean,
+  protocol: String,
+  restrictions: [String],
+  serviceFeeUrl: String,
+  algorithm: String,
+  operatingSystem: String,
+  licence: String,
+  purpose: String,
+  version: String,
+  subType: String,
+  startDate: Date,
+  finishDate: Date,
+  trainingAvailable: Boolean,
+  contactOrganisation: String,
+  contactEmail: String,
+  contactPhone: String,
+  capabilities: [String],
+  availableForUse: String,
+  contactUrl: String,
+  // Add any other fields as necessary
+  value: Number // Example of a numerical value field
+}, {
+  timestamps: true // Adds createdAt and updatedAt timestamps
+});
+
+// Asset Model Creation
+const Asset = mongoose.model('Asset', assetSchema);
+
+// Exporting the model
+module.exports = Asset;
