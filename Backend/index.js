dc452140c0ef515bddccbe0e6b85f8214a3b3be4
Leandru Martin <leandru.martin@slu.edu>
2024-03-27 10:42:35 -0500

    Importing and using assets router in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,13 @@
 import { PORT } from "./config.js";
 import express from "express";
+import AssetsRouter from "./routes/assets.js";
 const app = express();
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello, RAM Team!</h1>");
 });
+
+app.use("/assets", AssetsRouter);
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
