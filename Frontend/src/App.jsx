18a1d3c2ca005cc1af79d027a63191243fc0d975
ANANTHA PUNEETH CHOWDARY <96817637+puneeth1501@users.noreply.github.com>
2024-04-05 09:11:31 -0500

    updated profile icon in header component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,21 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import AssetManagement from "./components/AssetManagement";
-
+import { AuthProvider } from "./components/AuthContext";
 import Footer from "./components/Footer";
 
 function App() {
 	return (
-		<Router>
-			<div className="App">
-				<Header />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/loggedIn" component={AssetManagement} />
-				</Switch>
-				<Footer />
-			</div>
-		</Router>
+<AuthProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/loggedIn" component={AssetManagement} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
 	);
 }
 
