9db40a47fee2d2a5262a50be09fb79688d3bc3e6
nani1606 <krishnasyamala3@gmail.com>
2024-04-05 09:34:51 -0500

    user profiles

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,22 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import AssetManagement from "./components/AssetManagement";
+import UserProfile from "./components/UserProfile"; // Import UserProfile component
 
 import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Header />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/loggedIn" component={AssetManagement} />
-				</Switch>
-				<Footer />
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/loggedIn" component={AssetManagement} />
+          <Route exact path="/profile" component={UserProfile} /> {/* Add route for UserProfile */}
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
