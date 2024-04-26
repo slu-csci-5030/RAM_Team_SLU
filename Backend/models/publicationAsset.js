ed799066593ed7b231022912dceb0287c6e4f0c4
Sravya-Padala <padalasravya1508@gmail.com>
2024-04-26 11:39:53 -0500

    Add publication asset routes and fields

--- Backend/models/publicationAsset.js
+++ Backend/models/publicationAsset.js
@@ -2,22 +2,48 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const mySchema = new Schema({
-	// Other fields can be added here
-	types: {
-		type: String,
-		enum: [
-			"Artefact",
-			"Book",
-			"Chapter",
-			"Composition",
-			"Conference",
-			"Dataset",
-			"Design",
-		], // Specify the allowed values here
+const publicationSchema = new Schema(
+	{
+		category: {
+			type: String,
+			enum: [
+				"Artifact",
+				"Book",
+				"Chapter",
+				"Composition",
+				"Conference",
+				"Dataset",
+				"Design",
+			], // Specify the allowed values here
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		"additional-name": {
+			type: String,
+			required: true,
+		},
+		"coded-in": {
+			type: String,
+			required: function () {
+				return this.Category === "Database" || this.Category === "Software";
+			},
+		},
+		contacts: {
+			type: String,
+			enum: ["Person 1", "Person 2", "Person 3"],
+			required: true,
+		},
 	},
-});
+	{
+		timestamps: true,
+	});
 
-const MyModel = mongoose.model("MyModel", mySchema);
+const MyModel = mongoose.model("MyModel", publicationSchema);
 
 module.exports = MyModel;
